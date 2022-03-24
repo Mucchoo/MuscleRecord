09c714fa25d67c285e2d053663900fcbb6056d97
github user name <yazujumusa@gmail.com>
2022-03-24 16:08:37 +0900

    Initial Commit

---
+++ MuscleRecord/MuscleRecordApp.swift
@@ -0,0 +1,17 @@
+//
+//  MuscleRecordApp.swift
+//  MuscleRecord
+//
+//  Created by Musa Yazuju on 2022/03/24.
+//
+
+import SwiftUI
+
+@main
+struct MuscleRecordApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
